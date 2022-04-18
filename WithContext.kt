afce54a90f84fe377884cbe2dee7aabf3e3d1e7a
Koalnchinathan <70023481+kolanch@users.noreply.github.com>
2022-04-18 18:39:04 +0530

    Update WithContext.kt

--- WithContext.kt
+++ WithContext.kt
@@ -4,13 +4,13 @@ fun main(){
 
     runBlocking {
         launch {
-            println("name is ${Thread.currentThread().name}")
-            withContext(Dispatchers.IO){
+            println("name is ${Thread.currentThread().name}")            // run on main thread
+            withContext(Dispatchers.IO){                                  // change another thread
                 println("name is ${Thread.currentThread().name}")
-                withContext(Dispatchers.Unconfined){
-                    println("name is ${Thread.currentThread().name}")
+                withContext(Dispatchers.Unconfined){                      // change another thread unconfined   
+                    println("name is ${Thread.currentThread().name}")     // first run IO thread
                     delay(1000)
-                    println("name is ${Thread.currentThread().name}")
+                    println("name is ${Thread.currentThread().name}")     // then change another thread
                 }
             }
         }
